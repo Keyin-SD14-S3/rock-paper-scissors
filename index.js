e0a8b7f4a9870eb7b32942c39ec7de35b718a733
Matthew English <matt.english4@gmail.com>
2025-01-16 09:41:02 -0330

    Initial commit
    - Added the rock, paper, scissors game

---
+++ index.js
@@ -0,0 +1,35 @@
+//node index.js scissors
+const process = require('process');
+
+const validOptions = ['rock', 'paper', 'scissors'];
+const arguments = process.argv.slice(2);
+const userChoice = arguments[0];
+const computerChoice = validOptions[Math.floor(Math.random() * validOptions.length)];
+
+function determineWinner(playerInput, computerInput)  {
+    //Rock beats scissors
+    //Scissors beats paper
+    //Paper beats rock
+
+    if (playerInput === computerInput) {
+        return "It's a tie!";
+    }
+
+    if (
+        (playerInput === 'scissors' && computerInput === 'paper') ||
+        (playerInput === 'rock' && computerInput === 'scissors') ||
+        (playerInput === 'paper' && computerInput === 'rock')  
+    ) {
+        return "You win!";
+    }
+
+    return "Computer wins!";
+}
+
+if (!validOptions.includes(userChoice)) {
+    console.log('Usage: node index.js <rock|paper|scissors>');
+    return;
+}
+
+console.log(`You chose ${userChoice}, the computer chose ${computerChoice}`);
+console.log(determineWinner(userChoice, computerChoice));
